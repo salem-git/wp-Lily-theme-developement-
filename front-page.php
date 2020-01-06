e6a5d9e9971d9f0b9ba1699197ea8e9486cef4d2
salem <salem.gech@gmail.com>
2020-01-06 21:52:57 +0300

    Header image ssection added

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="showcase">
+    <div class="container">
+        <h1> Custom Bootstrap Wordpress Theme</h1>
+        <p> Sociis natoque penatibus et marign dis parturient </p>
+        <a class="btn btn-primary btn-lg">Read More</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
