7e9cfbc7c9fa3b019b49752852a3e710bb00a45d
salem <salem.gech@gmail.com>
2020-01-06 23:46:43 +0300

    Header Image inserted with customizable image,title,text and button

--- front-page.php
+++ front-page.php
@@ -1,10 +1,19 @@
 <?php get_header(); ?>
+<html>
+    <head>
+        <style>
+        .showcase{
+            background:url(<?php echo get_theme_mod('showcase_image',get_bloginfo('template_url').'../assets/images/gebeya.jfif'); ?>) no-repeat center center;
+        }
+        </style>
+    </head>
+</html>
 
 <section class="showcase">
     <div class="container">
-        <h1> Custom Bootstrap Wordpress Theme</h1>
-        <p> Sociis natoque penatibus et marign dis parturient </p>
-        <a class="btn btn-primary btn-lg">Read More</a>
+        <h1> <?php echo get_theme_mod('showcase_heading','Custom Bootstrap Wordpress Theme'); ?></h1>
+        <p><?php echo get_theme_mod('showcase_text',' Sociis natoque penatibus et marign dis parturient'); ?> </p>
+        <a href="<?php echo get_theme_mod('btn_url','http://test.com'); ?>" class="btn btn-primary btn-lg"><?php echo get_theme_mod('btn_text','Read More'); ?></a>
     </div>
 </section>
 
--- inc/customizer.php
+++ inc/customizer.php
@@ -25,6 +25,72 @@ function wp_lily_customize_register( $wp_customize ) {
 			'render_callback' => 'wp_lily_customize_partial_blogdescription',
 		) );
 	}
+
+	//Showcase Section
+
+	$wp_customize->add_section('showcase',array(
+		'title' => __('Showcase','wp_lily'),
+		'description' => sprintf(__('Options for showcase','wp_lily')),
+		'priority' => 130
+	));
+
+	$wp_customize->add_setting('showcase_image',array(
+		'default' => get_bloginfo('template_directory').'/assets/img/gebeya.jfif',
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize,'showcase_image',array(
+		'label' => __('Showcase Image','wp_lily'),
+		'section'=>'showcase',
+		'settings' => 'showcase_image',
+		'priority' => 1
+	)));
+
+	$wp_customize->add_setting('showcase_heading',array(
+		'default' => _X('Custom Bootstrap Wordpress Theme','wp_lily'),
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('showcase_heading',array(
+		'label' => __('Heading','wp_lily'),
+		'section'=>'showcase',
+		'priority'=>2
+	));
+
+	$wp_customize->add_setting('showcase_text',array(
+		'default' => _X('Sociis natoque penatibus et marign dis parturient','wp_lily'),
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('showcase_text',array(
+		'label' => __('Text','wp_lily'),
+		'section'=>'showcase',
+		'priority'=>3
+	));
+
+
+	$wp_customize->add_setting('btn_url',array(
+		'default' => _X('http://test.com','wp_lily'),
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('btn_url',array(
+		'label' => __('Button URL','wp_lily'),
+		'section'=>'showcase',
+		'priority'=>4
+	));
+
+
+	$wp_customize->add_setting('btn_text',array(
+		'default' => _X('Read More','wp_lily'),
+		'type' => 'theme_mod'
+	));
+
+	$wp_customize->add_control('btn_text',array(
+		'label' => __('Button Text','wp_lily'),
+		'section'=>'showcase',
+		'priority'=>5
+	));
 }
 add_action( 'customize_register', 'wp_lily_customize_register' );
 
