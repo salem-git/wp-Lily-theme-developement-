a745d6b03e3bd57fe7f72aa384af0f078a3db52c
salem <salem.gech@gmail.com>
2020-01-20 15:36:07 +0300

    Header image is removed in 404 and blog and i tried to add my own image in 404 but not successfull

--- 404.php
+++ 404.php
@@ -5,6 +5,19 @@
  */
  
 get_header(); ?>
+<html>
+<head>
+<style>
+
+.page-header{
+    background: url("assets/image/question.jpg");
+}
+
+</style>
+</head>
+
+</html>
+
 <div class="container">
 <div class="headerImage"></div>
   <div class="row">
@@ -13,7 +26,9 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <div id="content" class="site-content" role="main">
  
+
             <header class="page-header">
+            <!-- <img src="/assets/img/question.jpg" alt="question "/> -->
                 <h1 class="page-title"><?php _e( 'Not found', 'twentythirteen' ); ?></h1>
             </header>
  
@@ -25,7 +40,7 @@ get_header(); ?>
                     <?php get_search_form(); ?>
 					<hr>
                     
-
+44
 <div class="col3">
 <div class="col-header">         
 <h5>Check out some of our popular content:</h5>
