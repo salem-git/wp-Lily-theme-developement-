4cfe643485c604f8cb131848ed3a6d3cebc1392a
salem <salem.gech@gmail.com>
2020-01-21 12:18:43 +0300

    the excerpt and the content modified for single.php in content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,7 +13,10 @@
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
+			
 			the_title( '<h1 class="entry-title">', '</h1>' );
+			// the_content();
+			
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
@@ -33,8 +36,9 @@
 
 	<div class="entry-content">
 		<?php
+		if ( is_singular() ) :
 		
-		the_excerpt( sprintf(
+		the_content( sprintf(
 			wp_kses(
 				/* translators: %s: Name of current post. Only visible to screen readers */
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wp-lily' ),
@@ -46,12 +50,26 @@
 			),
 			get_the_title()
 		) );
+		else:
+			the_excerpt( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wp-lily' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
 
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-lily' ),
 			'after'  => '</div>',
 		) );
 		?>
+		<?php endif; ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
