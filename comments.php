82279807cb1ca400592f0deee7458c756256050d
salem <salem.gech@gmail.com>
2020-01-21 10:37:24 +0300

    Custom comment added in comment.php

--- comments.php
+++ comments.php
@@ -1,75 +1,50 @@
-<?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package wp-Lily
- */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if ( post_password_required() ) {
-	return;
-}
+
+<div class="comments">
+<h2>Comments</h2>
+<?php $args = array(
+'walker'   => null,
+'max_depth' => '',
+'style'     => 'ul',
+'callback'  => null,
+'end-callback'  => null,
+'type'   => 'all',
+'reply_text' =>  'Reply',
+'page'   => '',
+'per_page'   => '',
+'avatar_size' => 80,
+'reverse_top_level' => null,
+'reverse_children'  => '',
+'format'   =>  'html5',
+'short_ping'   => false,
+'echo' => true
+  
+);?>
+
+<?php wp_list_comments( $args,$comments );
+
+$comments_args = array(
+	'label_submit' => 'Send',
+	'title_reply' => 'Write a Reply or Comment',
+	'comment_notes_after' => '',
+	'comment_field' => '<p class="comment-form-comment"><label for=""comment">'._x('Comment','noun').
+	'</label><br/><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+
+);
+
+comment_form($comment_args);
+
+
+
 ?>
 
-<div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$wp_lily_comment_count = get_comments_number();
-			if ( '1' === $wp_lily_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'wp-lily' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $wp_lily_comment_count, 'comments title', 'wp-lily' ) ),
-					number_format_i18n( $wp_lily_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
-			?>
-		</ol><!-- .comment-list -->
 
-		<?php
-		the_comments_navigation();
+</div>
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wp-lily' ); ?></p>
-			<?php
-		endif;
 
-	endif; // Check for have_comments().
 
-	comment_form();
-	?>
 
-</div><!-- #comments -->
+
