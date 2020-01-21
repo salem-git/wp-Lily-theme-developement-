7bb6cb2f63b2f8700dbf18a640d3f183d4b0deaf
salem <salem.gech@gmail.com>
2020-01-21 12:38:39 +0300

    content-aside.php added for aside post format

--- archive.php
+++ archive.php
@@ -9,10 +9,13 @@
 
 get_header();
 ?>
+
+
 <div class="container">
 <div class="headerImage"></div>
   <div class="row">
     <div class="col-sm-8">
+	
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -35,7 +38,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
 
--- index.php
+++ index.php
@@ -30,7 +30,7 @@ get_header();
             ?>
             <header>
               <h1 class="page-title screen-reader-text">
-              
+              <a href="<?php the_permalink(); ?>"><?php single_post_title(); ?></a>
               </h1>
             </header>
             <?php
@@ -45,7 +45,7 @@ get_header();
             * If you want to override this in a child theme, then include a file
             * called content-___.php (where ___ is the Post Type name) and that will be used instead.
             */
-            get_template_part( 'template-parts/content', get_post_type() );
+            get_template_part( 'template-parts/content', get_post_format() );
 
           endwhile;
 
