6121271b0ae0493e06b80cc7e877daa82e4c4c99
salem <salem.gech@gmail.com>
2020-01-28 11:55:20 +0300

    sidebar widget made in card view

--- sidebar.php
+++ sidebar.php
@@ -11,10 +11,17 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
+<div class="card mb-4" id="widget-area">
+<div class="card-body">
 <?php
 if(!is_front_page()) : ?>
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
 
+</div>
+</div>
+
+      
+         
 <?php endif; ?>
